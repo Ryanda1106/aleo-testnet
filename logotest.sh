29abdc6682633a55f501cc674ada1d22216bdb6e
Rianda Dalimunte <59490489+Ryanda1106@users.noreply.github.com>
2023-07-23 07:14:09 +0700

    Create logotest.sh

---
+++ logotest.sh
@@ -0,0 +1,9 @@
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+echo -e '\e[33mProject =\e[37m' Aleo Testnet
+echo -e '\e[33mCommunity =\e[37m' BlackMadia
+echo -e '\e[33mTelegram Channel =\e[37m' https://t.me/ryanda1106
+echo -e '\e[33mTwitter =\e[37m' https://twitter.com/BlackMadia
+echo -e "\e[0m"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
